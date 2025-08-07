64a518bdb8a12e673ccee1fb23403c685b9e4070
Angel <angel10diez03@gmail.com>
2025-08-07 12:24:28 -0600

    feat: se agregan validaciones en DTOs de Medicos

---
+++ SaludGest/DTOs/Medico/MedicoCreateDTO.cs
@@ -0,0 +1,37 @@
+﻿using SaludGest.DTOs.Registro;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaludGest.DTOs.Medico
+{
+    public class MedicoCreateDTO : RegistroDTO
+    {
+        public int MedicoId { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido paterno es obligatorio.")]
+        [Display(Name = "Apellido Paterno")]
+        public string ApellidoPaterno { get; set; }
+
+        [Required(ErrorMessage = "El apellido materno es obligatorio.")]
+        [Display(Name = "Apellido Materno")]
+        public string ApellidoMaterno { get; set; }
+
+        [Required(ErrorMessage = "La especialidad es obligatoria.")]
+        [Display(Name = "Especialidad")]
+        public int EspecialidadId { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio.")]
+        [Display(Name = "Teléfono")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El departamento es obligatorio.")]
+        [Display(Name = "Departamento")]
+        public int DepartamentoId { get; set; }
+    }
+}
---
+++ SaludGest/DTOs/Medico/MedicoReadDTO.cs
@@ -0,0 +1,28 @@
+﻿using SaludGest.DTOs.Registro;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaludGest.DTOs.Medico
+{
+    public class MedicoReadDTO : RegistroDTO
+    {
+        public int MedicoId { get; set; }
+        [Display(Name = "Nombre Completo")]
+        public string NombreCompleto { get; set; }
+
+        [Display(Name = "ID de Especialidad")]
+        public int EspecialidadId { get; set; }
+
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+        [Display(Name = "ID de Departamento")]
+        public int DepartamentoId { get; set; }
+
+        [Display(Name = "Especialidad")]
+        public string EspecialidadNombre { get; set; }
+
+        [Display(Name = "Departamento")]
+        public string DepartamentoNombre { get; set; }
+
+    }
+}
