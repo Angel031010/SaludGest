ea9342ce9aa3479618238085c0b6561a7fd252ac
Angel <angel10diez03@gmail.com>
2025-08-07 12:16:37 -0600

    feat: se agregan validaciones en DTOs de Especialidades

---
+++ SaludGest/DTOs/Especialidad/EspecialidadCreateDTO.cs
@@ -0,0 +1,18 @@
+﻿using SaludGest.DTOs.Registro;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaludGest.DTOs.Especialidad
+{
+    public class EspecialidadCreateDTO : RegistroDTO
+    {
+        public int EspecialidadId { get; set; }
+        [Required(ErrorMessage = "El nombre de la especialidad es obligatorio.")]
+        [Display(Name = "Nombre de Especialidad")]
+        public string NombreEspecialidad { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+    }
+
+}
---
+++ SaludGest/DTOs/Especialidad/EspecialidadReadDTO.cs
@@ -0,0 +1,14 @@
+﻿using SaludGest.DTOs.Registro;
+using System.ComponentModel.DataAnnotations;
+namespace SaludGest.DTOs.Especialidad
+{
+    public class EspecialidadReadDTO : RegistroDTO
+    {
+        public int EspecialidadId { get; set; }
+        [Display(Name = "Nombre de Especialidad")]
+        public string NombreEspecialidad { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+    }
+}
