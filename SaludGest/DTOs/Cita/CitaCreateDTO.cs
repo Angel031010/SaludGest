d64b081942f709d0439e3c44e108b460eeab75a9
Angel <angel10diez03@gmail.com>
2025-08-07 12:36:49 -0600

    feat: se agregan validaciones en DTOs de Citas

---
+++ SaludGest/DTOs/Cita/CitaCreateDTO.cs
@@ -0,0 +1,28 @@
+﻿using SaludGest.DTOs.Registro;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaludGest.DTOs.Cita
+{
+    public class CitaCreateDTO : RegistroDTO
+    {
+        public int CitaId { get; set; }
+        [Required(ErrorMessage = "La fecha y hora de la cita son obligatorias.")]
+        [Display(Name = "Fecha y Hora")]
+        public DateTime FechaHora { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar un médico.")]
+        [Display(Name = "Médico")]
+        public int MedicoId { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar un paciente.")]
+        [Display(Name = "Paciente")]
+        public int PacienteId { get; set; }
+
+        [Required(ErrorMessage = "El estado de la cita es obligatorio.")]
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+
+        [Display(Name = "Observaciones")]
+        public string Observaciones { get; set; }
+    }
+}
---
+++ SaludGest/DTOs/Cita/CitaReadDTO.cs
@@ -0,0 +1,30 @@
+﻿using SaludGest.DTOs.Registro;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaludGest.DTOs.Cita
+{
+    public class CitaReadDTO : RegistroDTO
+    {
+        public int CitaId { get; set; }
+        [Display(Name = "Fecha y Hora")]
+        public DateTime FechaHora { get; set; }
+
+        [Display(Name = "ID Médico")]
+        public int MedicoId { get; set; }
+
+        [Display(Name = "Nombre del Médico")]
+        public string NombreMedico { get; set; }
+
+        [Display(Name = "ID Paciente")]
+        public int PacienteId { get; set; }
+
+        [Display(Name = "Nombre del Paciente")]
+        public string NombrePaciente { get; set; }
+
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+
+        [Display(Name = "Observaciones")]
+        public string Observaciones { get; set; }
+    }
+}
